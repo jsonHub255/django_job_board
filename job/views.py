7d2c67dfc40b065421d7db773f2340bf3112fdd2
Json <jsonlli255@gmail.com>
2021-04-25 22:24:01 +0000

    Add Category models and Foreignkey category on job model and set urls.py

--- job/views.py
+++ job/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Job, Category
+from django.http import HttpResponse
 # Create your views here.
+
+def job_list(request):
+    return HttpResponse("LIST OF JOBS HERE")
+
+def job_details(request, id):
+    pass
