14849bbb5d84033ae393daaf68e053e00a138df4
Json <jsonlli255@gmail.com>
2021-04-25 21:11:26 +0000

    Add fileds for Job model

--- job/models.py
+++ job/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+JOB_TYPE = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+)
 class Job(models.Model): # Create a table `Job`
     title = models.CharField(max_length=100) # Create table field `tile`
+    # location
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return self.title
+
     
