7388abca5667cad9a304b10cecfb03c0e3dbaccc
Json <jsonlli255@gmail.com>
2021-04-25 01:31:39 +0000

    add model Joband fild title on it.

--- job/models.py
+++ job/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model): # Create a table `Job`
+    title = models.CharField(max_length=100) # Create table field `tile`
+    
