c2757dd1b7c4a8ae4203285d815e9882f2f8c582
Json <jsonlli255@gmail.com>
2021-04-27 23:45:38 +0000

    add pagination in Job_list page

--- job/views.py
+++ job/views.py
@@ -1,3 +1,5 @@
+from django.core import paginator
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Job, Category
 from django.http import HttpResponse
@@ -5,7 +7,10 @@ from django.http import HttpResponse
 
 def job_list(request):
     job_list = Job.objects.all()
-    context = {'jobs': job_list}
+    paginator = Paginator(job_list, 1)  # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'jobs': page_obj, 'jobs2': job_list}
     return render(request, "job/job_list.html", context)
 
 def job_details(request, id):
